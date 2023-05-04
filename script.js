1bc3218a0792dc31202b3eb1a24d8e19ee799b5a
Carolina Zapata <zapata.cdla@gmail.com>
2023-05-03 22:57:00 -0300

    Creada la lógica de des/encriptación según instrucciones del video, agregados eventos de botones en index.html

---
+++ script.js
@@ -0,0 +1,48 @@
+const textArea = document.querySelector(".textArea");
+const mensaje = document.querySelector(".mensaje");
+
+/*
+    e --> enter
+    i --> imes
+    a --> ai
+    o --> ober
+    u --> ufat
+
+*/
+
+function botonEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function botonDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function encriptar(stringEncriptado){
+    let llavesEncript = [["e", "enter"], ["i", "imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for(let i = 0; i < llavesEncript.length ; i++){
+        if(stringEncriptado.includes(llavesEncript[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(llavesEncript[i][0], llavesEncript[i][1]);
+        }
+    }
+    return stringEncriptado;
+}
+
+function desencriptar (stringDesencriptado){
+    let llavesEncript = [["e", "enter"], ["i", "imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for(let i = 0; i < llavesEncript.length ; i++){
+        if(stringDesencriptado.includes(llavesEncript[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(llavesEncript[i][1], llavesEncript[i][0]);
+        }
+    }
+    return stringDesencriptado;
+}
