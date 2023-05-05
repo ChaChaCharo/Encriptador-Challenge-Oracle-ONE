80f6926e1c76c2c63996731717c59d60301a2079
Carolina Zapata <63057121+ChaChaCharo@users.noreply.github.com>
2023-05-04 21:43:58 -0300

    Eliminado código de ejemplo

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 const textArea = document.querySelector(".textArea");
 const mensaje = document.querySelector(".mensaje");
 
-/*
-    e --> enter
-    i --> imes
-    a --> ai
-    o --> ober
-    u --> ufat
-
-    function copyText() {
-            
-            //leemos el texto de una area determinada
-            var text = document.getElementById("dummy-text").innerHTML;
-            //En este ejemplo solamente tenemos texto por lo que podríamos usar .innerText
-            //Pero supongamos que tuvieramos algo mas que texto, como bloques de codigo,
-            //entonces sería mejor usar .innerHTML
-
-            //ahora usamos navigator object
-
-            navigator.clipboard.writeText(text);
-
-        } */
-
 function botonEncriptar() {
     const textoEncriptado = encriptar(textArea.value);
     mensaje.value = textoEncriptado;
